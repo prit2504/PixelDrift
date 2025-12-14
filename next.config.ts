3c81f1a5101a725d28890cce554655d1c7d63c63
Prit K Panchal <ppanchal2504@gmail.com>
2025-12-14 10:52:11 +0530

    updated api

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: [
       "https://pixeldriftapi.onrender.com",
+      "https://pixeldrift-backend-prit25042821-8gsem2ki.leapcell.dev"
     ],
 };
 
